62bca66bc85650c94cf2bb3d60ece8ca52071cd5
TORY-SKY <ezevictor559@gmail.com>
2024-01-11 23:00:46 +0100

    working on the dropdown menu

--- PlatformPage.js
+++ PlatformPage.js
@@ -3,3 +3,14 @@
 //     faq.classList.toggle('active');
 // }));
 // alert("hello world");
+let header = document.querySelector('.Header');
+let menu = document.querySelector('.Menu');
+let dropDownContent = document.querySelector('.DroppDownMenuu');
+let clickCount  = 0;
+menu.addEventListener('click', ()=>{
+    if(dropDownContent.style.display === "flex"){
+        dropDownContent.style.display = "none";
+    }else{
+        dropDownContent.style.display = 'block';
+    }
+});
